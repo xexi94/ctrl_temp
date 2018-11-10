5f21f0256bd5071bb68f5c8ed37a374a2baa3982
xexi94 <sergi.domingo94@gmail.com>
2018-11-10 18:35:19 +0100

    18k1006

--- ctrl_temp.py
+++ ctrl_temp.py
@@ -56,140 +56,135 @@ class MainPage(tk.Frame):
        
     def __init__(self, parent, controller):
         
-        #DEVICE SELECTION
-        def dev_select():
-            global optionList
-            optionList=["     "]
-                
-            if os.name == 'nt':
-                None
-                        
-            elif os.name == 'posix': 
-                lines = os.popen("dmesg | grep tty").readlines()
-                for i in lines:
-                    aux = i[i.find('tty')::]
-                    device = aux[0:(aux.find(" "))-1]
-                    statment = str((os.popen("test -e /dev/"+device+" && echo True || echo False").readlines()))
-                    if (statment[2:-4] == "True"):
-                        
-                        optionList.append(device)
-                    else:
-                        optionList.append("     ")
-                    
-                optionList = sorted(list(set(optionList)))
-            
-            return (optionList)    
-          
-        #DATA ACQUISITION
-        def scanning():
-            
-            if running:
-                try:
-                    data = ser.readline()
-                    if data:
-                        print (int(data[:-1]))
-                        f.write(str(int(data[:-1]))+"\n")
-                except (NameError,FileNotFoundError):
-                    print("NameError")
-            else:
-                print("NaN")
-            self.after(500, scanning)   
-                    
-        def start():
-            global running
-            global ser
-            global f
-            running = True
-            if os.name == 'nt':
-                    None
-            elif os.name == 'posix':
-                
-            
-                dev=str(self.option.get())
-                dir_path = os.path.dirname(os.path.realpath(__file__))
-                ser = serial.Serial(str('/dev/'+dev), 9600)                    
-                path = dir_path+"/tempfiles"
-                    
-                f = open(path+"/temp01.txt","w")
-        
-        def stop():
-            global running
-            global ser
-            global f
-            running = False
-            try:
-                f.close()
-            except (NameError,FileNotFoundError):
-                print("NameError")
-            ser.close()
-            popupmsg()
-        
-        def popupmsg():
-
-            def yes():
-                if os.name == 'nt':
-                    None
-                elif os.name == 'posix':
-                    dir_path = os.path.dirname(os.path.realpath(__file__))
-                    l = open(dir_path+"/tempfiles/temp01.txt",'r')
-                    a=l.readlines()
-                    q = filedialog.asksaveasfile(initialdir="./",title="Selec Folder",filetypes = (("text files","*.txt"),("all files","*.*")))
-                    for i in range(len(a)):
-                        q.write(str(a[i]))
-                    q.close()
-                    
-                    os.system("rm "+dir_path+"/tempfiles/temp01.txt")
-                    popup.destroy()
-            def no():
-                if os.name == 'nt':
-                    None
-                elif os.name == 'posix':
-                    dir_path = os.path.dirname(os.path.realpath(__file__))
-                    os.system("rm "+dir_path+"/tempfiles/temp01.txt")
-                    popup.destroy()
-
-            popup = tk.Tk()
-            popup.resizable(0,0)
-            popup.geometry("200x100")
-            if os.name == 'nt':
-                None
-            elif os.name == 'posix':
-                None
-            popup.wm_title("")
-            message=tk.Label(popup,text="description")
-            Ys = tk.Button(popup,text="Yes",command=yes)
-            Bl = tk.Button(popup,text="No",command=no)
-            message.grid()
-            Ys.grid()
-            Bl.grid()
-            popup.mainloop()
-                 
         
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        label = tk.Label(self, text="This is the main page")#, font=controller.title_font
-        label.grid()
         
-        global running
-        running = False
         
+        
+        frame1 = tk.Frame(self,height=2,width=5,bg="red")
+        frame1.grid(row = 0, column = 0, sticky = tk.W)
+        frame1.label = tk.Label(self, text="This is the main page")#, font=controller.title_font
+        frame1.label.grid(row=0, column=0)
         #BUTTONS
-        dev_select()
+        self.dev_select()
+        
+        
         self.option = tk.StringVar(self)
         self.option.set(optionList[0])
         w=tk.OptionMenu(self,self.option,*optionList)
         w.grid()
         
-        button1 = tk.Button(self, text="Start",command=start)
-        button2 = tk.Button(self, text="STOP",command=stop)
+        button1 = tk.Button(self, text="Start",command=self.start)
+        button2 = tk.Button(self, text="STOP",command=self.stop)
         button1.grid()
         button2.grid()
-
-        scanning()
-     
         
+        global running
+        running = False
+        
+        self.scanning()
+    def dev_select(self):
+        global optionList
+        optionList=["     "]
+                
+        if os.name == 'nt':
+            None
+                        
+        elif os.name == 'posix': 
+            lines = os.popen("dmesg | grep tty").readlines()
+            for i in lines:
+                aux = i[i.find('tty')::]
+                device = aux[0:(aux.find(" "))-1]
+                statment = str((os.popen("test -e /dev/"+device+" && echo True || echo False").readlines()))
+                if (statment[2:-4] == "True"):
+                        
+                    optionList.append(device)
+                else:
+                    optionList.append("     ")
+                    
+            optionList = sorted(list(set(optionList))) 
+    def scanning(self):
+            
+        if running:
+            try:
+                data = ser.readline()
+                if data:
+                    print (int(data[:-1]))
+                    f.write(str(int(data[:-1]))+"\n")
+            except (NameError,FileNotFoundError):
+                print("NameError")
+        else:
+            print("NaN")
+        
+        self.after(500, self.scanning)   
+    
+    def start(self):
+        global running
+        global ser
+        global f
+        running = True
+        if os.name == 'nt':
+            None
+        elif os.name == 'posix':
+                
+            dev=str(self.option.get())
+            dir_path = os.path.dirname(os.path.realpath(__file__))
+            ser = serial.Serial(str('/dev/'+dev), 9600)                    
+            path = dir_path+"/tempfiles"
+                    
+            f = open(path+"/temp01.txt","w")
+    def stop(self):
+        global running
+        global ser
+        global f
+        running = False
+        try:
+            f.close()
+        except (NameError,FileNotFoundError):
+            print("NameError")
+        ser.close()
+        self.popupmsg()
 
+    def popupmsg(self):
 
+        def yes():
+            if os.name == 'nt':
+                None
+            elif os.name == 'posix':
+                dir_path = os.path.dirname(os.path.realpath(__file__))
+                l = open(dir_path+"/tempfiles/temp01.txt",'r')
+                a=l.readlines()
+                q = filedialog.asksaveasfile(initialdir="./",title="Selec Folder",filetypes = (("text files","*.txt"),("all files","*.*")))
+                for i in range(len(a)):
+                    q.write(str(a[i]))
+                q.close()
+                    
+                os.system("rm "+dir_path+"/tempfiles/temp01.txt")
+                popup.destroy()
+        def no():
+            if os.name == 'nt':
+                None
+            elif os.name == 'posix':
+                dir_path = os.path.dirname(os.path.realpath(__file__))
+                os.system("rm "+dir_path+"/tempfiles/temp01.txt")
+                popup.destroy()
+
+        popup = tk.Tk()
+        popup.resizable(0,0)
+        popup.geometry("200x100")
+        if os.name == 'nt':
+            None
+        elif os.name == 'posix':
+            None
+        popup.wm_title("")
+        message=tk.Label(popup,text="description")
+        Ys = tk.Button(popup,text="Yes",command=yes)
+        Bl = tk.Button(popup,text="No",command=no)
+        message.grid()
+        Ys.grid()
+        Bl.grid()
+        popup.mainloop()
 
 if __name__ == "__main__":
     app = SampleApp()
